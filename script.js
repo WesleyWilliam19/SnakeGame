b3b192fed22f6604103061417578c71016a81b92
wesleywilliam19 <wesley.william1@outlook.com>
2021-08-24 20:23:48 -0300

    Criando Movimentos

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 32; //Tamanho
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 /*Background*/
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha(){
-    for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+/*Snake*/
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "purple";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    // Movimentação
+    if(direction == "right") snakeX += box; //Se direção = right snakeX acrescenta +1 box
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box; 
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); //Renova a cada 100ms
